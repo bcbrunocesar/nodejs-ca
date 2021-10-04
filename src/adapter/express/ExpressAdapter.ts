9fc69c249a1b7601e69a60f2903f21793a2bad1d
Bruno César <bc_farias@hotmail.com>
2021-10-03 23:34:06 -0300

    Remover comentário do adapter

--- src/adapter/express/ExpressAdapter.ts
+++ src/adapter/express/ExpressAdapter.ts
@@ -28,26 +28,3 @@ export const expressAdapter = function (moduleFn: any, httpContextFn?: any) {
     }
   };
 };
-
-// export default class ExpressAdapter {
-// public static create =
-//   (fn: any) => async (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//       const { status, data, errors, message }: HttpResponseBase = await fn(
-//         req.params,
-//         req.body
-//       );
-//       if (errors) {
-//         res.status(status).json({ message, errors });
-//         return;
-//       }
-//       if (data) {
-//         res.status(status).json({ message, data });
-//         return;
-//       }
-//       res.status(status).json();
-//     } catch (e) {
-//       console.error('[ERROR] EXPRESS ADAPTER');
-//       next(e);
-//     }
-//   };
